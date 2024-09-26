2e9105fa3a089a875032cc628dd00c129ee3db88
SvitLanaTy <stymchenko@gmail.com>
2024-09-26 21:18:47 +0200

    add rout GET

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import cors from 'cors';
 import express from 'express';
 import pino from 'pino-http';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import { env } from './utils/env.js';
 
-// const PORT = Number(env('PORT', '3000'));
-const PORT = 3000;
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
@@ -25,9 +26,46 @@ export const setupServer = () => {
     });
   });
 
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    try {
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: 'Contact not found',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
   app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: error.message,
     });
   });
 
