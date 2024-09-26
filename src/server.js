a39e6120c2d594e6dca604df3108d59916377045
SvitLanaTy <stymchenko@gmail.com>
2024-09-26 20:42:01 +0200

    Add MongoDB and server connection

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import cors from 'cors';
+import express from 'express';
+import pino from 'pino-http';
+
+// const PORT = Number(env('PORT', '3000'));
+const PORT = 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
