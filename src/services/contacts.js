78c6673073d6450ad25d244545d5645eebf16650
SvitLanaTy <stymchenko@gmail.com>
2024-09-30 18:40:16 +0200

    Task 3-5

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,39 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { Contact } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
+export const getAllContacts = async ({
+  page,
+  perPage,
+  sortBy = 'name',
+  sortOrder = SORT_ORDER.ASC,
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const contactQuery = Contact.find();
+
+  if (typeof filter.isFavourite === 'boolean') {
+    contactQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const contactsCount = await Contact.find()
+    .merge(contactQuery)
+    .countDocuments();
+
+  const contacts = await contactQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
